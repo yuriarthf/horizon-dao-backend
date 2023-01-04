ebd398d97260a89087de64797e68988d6f938c0c
yuriarthf <yuriarthf@protonmail.com>
2023-01-04 12:03:42 +0700

    fix: correct database url

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,3 +1,3 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from "@config";
 
-export const dbConnection = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+export const dbConnection = `mongodb+srv://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
