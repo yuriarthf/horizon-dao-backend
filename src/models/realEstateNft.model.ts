78dfc60d474431c7d09625b7a036268e1628995b
yuriarthf <yuriarthf@protonmail.com>
2023-01-07 00:11:13 +0700

    feat: realEstateNft model

---
+++ src/models/realEstateNft.model.ts
@@ -0,0 +1,33 @@
+import { prop, getModelForClass, modelOptions } from "@typegoose/typegoose";
+
+
+@modelOptions({ schemaOptions: { timestamps: false, id: false } })
+class Attribute {
+  @prop({ type: String, required: true })
+  public value!: string;
+
+  @prop({ type: String, required: true })
+  public trait_type!: string;
+}
+
+@modelOptions({ schemaOptions: { collection: "RealEstateNFT", timestamps: false } })
+class RealEstateNFT {
+  @prop({ type: Number, required: true })
+  public _id!: number;
+
+  @prop({ type: String, trim: true })
+  public description?: string;
+
+  @prop({ type: String, trim: true })
+  public image?: string;
+
+  @prop({ type: [Attribute] })
+  public attributes?: Attribute[];
+
+  @prop({ type: String })
+  public external_url?: string;
+}
+
+const RealEstateNFTModel = getModelForClass(RealEstateNFT);
+
+export default RealEstateNFTModel;
