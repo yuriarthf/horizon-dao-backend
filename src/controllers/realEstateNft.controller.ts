58de81d81163e155e8d94704ea07233a24873fd4
yuriarthf <yuriarthf@protonmail.com>
2023-01-15 12:44:59 +0700

    fix: conform to OpenSea standard

--- src/controllers/realEstateNft.controller.ts
+++ src/controllers/realEstateNft.controller.ts
@@ -7,7 +7,7 @@ class RealEstateNftController {
   public getRealEstateNFt = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const realEstateNft = await this.realEstateNftService.getRealEstateNftById(req.params.realEstateNftId);
-      res.status(200).json({ data: realEstateNft });
+      res.status(200).json(realEstateNft);
     } catch (error) {
       next(error);
     }
